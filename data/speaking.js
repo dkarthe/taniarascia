0c2ecf77f0fe4f29b4c6f23ca8a311d340f64617
Karthik D <dkarthe@users.noreply.github.com>
2019-03-20 16:21:02 +0530

    Delete speaking.js

--- data/speaking.js
+++
@@ -1,9 +0,0 @@
-const speaking = [
-  {
-    title: 'Developing a WordPress Theme from Scratch',
-    path:
-      'https://wordpress.tv/2017/08/30/tania-rascia-how-to-develop-a-wordpress-theme-from-scratch/',
-  },
-]
-
-export default speaking
